ef5712a05f1369ba6610cb1f2ab0530940093fcd
801DevLabs <801DevLabs@gmail.com>
2019-02-07 07:55:00 -0700

    added loading the controller from the url

--- workingWithMvcFramework/app/libraries/core.php
+++ workingWithMvcFramework/app/libraries/core.php
@@ -1,19 +1,40 @@
 <?php
 
-  // * App Core Class
-  // * Creates URL & loads core controller
+  // * APP CORE CLASS
+  // * CREATES URL AND LOADS CORE CONTROLLER
   // * URL FORMAT - /controller/method/params
-  
+
   class Core {
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
 
     public function __construct(){
-      $this->getUrl();
+      //print_r($this->getUrl());
+
+      $url = $this->getUrl();
+
+      // LOOK IN CONTROLLERS FOR FIRST VALUE
+      if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
+        // IF EXSISTS, SET AS CONTROLLER
+        $this->currentController = ucwords($url[0]);
+        // UNSET 0 INDEX
+        unset($url[0]);
+      }
+
+      // REQUIRE THE CONTROLLER
+      require_once '../app/controllers/'. $this->currentController . '.php';
+
+      // INSTANTIATE CONTROLLER CLASS
+      $this->currentController = new $this->currentController;
     }
 
     public function getUrl(){
-      echo $_GET['url'];   
+      if(isset($_GET['url'])){
+        $url = rtrim($_GET['url'], '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
+      }
     }
   }
